aed293c982c2c0895a9c87a4327120e4bf9c70c8
ChaithanyaVoorukonda <138677532+ChaithanyaVoorukonda@users.noreply.github.com>
2025-02-06 20:22:24 +0530

    Calculator MSTesting

---
+++ Day8/Q1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Assignment1
+{
+    public class Calculator
+    {
+        public double Addition(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtraction(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+        public double Division(double x, double y)
+        {
+            if(y==0)
+            {
+                throw new DivideByZeroException("We cannot divide by zero");
+            }
+            return x / y;
+        }
+    }
+}
---
+++ Day8/Q1/Test1.cs
@@ -0,0 +1,46 @@
+﻿using Day8Assignment1;
+
+namespace Day8Assignment1MSTesting
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Addition(20, 3);
+            Assert.AreEqual(res, 23);
+        }
+        [TestMethod]
+        public void Sub()
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Subtraction(20, 30);
+            Assert.AreEqual(res, -10);
+        }
+        [TestMethod]
+        public void Mul()
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Multiplication(10, 4);
+            Assert.AreEqual(res, 40);
+        }
+        [TestMethod]
+        public void Div()
+        {
+
+            Calculator cal = new Calculator();
+            double res = cal.Division(30, 3);
+            Assert.AreEqual(10, res);
+
+        }
+        [TestMethod]
+        public void DivbyZero()
+        {
+            Calculator cal = new Calculator();
+            Assert.ThrowsException<DivideByZeroException>(() => cal.Division(10, 0));
+        }
+
+    }
+}
